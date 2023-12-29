e397fcf3ca7b369c82bf71d1fb32e259b5e94cff
Jibon <manvirjibon@gmail.com>
2023-12-29 18:37:42 +0600

    hover

--- src/Components/Photography.jsx
+++ src/Components/Photography.jsx
@@ -3,8 +3,9 @@ function Photography() {
   return (
    <section>
     <div className=" flex  justify-center mt-20">
-       <div className=" h-[300px]">
+       <div className=" h-[300px] relative">
        <img src="https://images.squarespace-cdn.com/content/v1/5b5617b9697a9878c6351a85/1604434623934-V4M6DMF2VV5627U4RR9D/Pavel_Bolo_Photography.jpg?format=1000w" alt="" className=" h-full" /> 
+       <p className=" absolute top-0 left-0">Photography</p>
        </div>
        <div className=" h-[300px]">
        <img src="https://images.squarespace-cdn.com/content/v1/5b5617b9697a9878c6351a85/1604434544631-T821ET958H8RNV5ROWO0/Cheetah.jpg?format=1000w" alt="" className=" h-full"  />
