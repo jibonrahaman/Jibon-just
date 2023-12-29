d0fe2f63a76de8e42d86fef18d483926f24b3bb9
Jibon <manvirjibon@gmail.com>
2023-12-29 19:06:20 +0600

    design done

--- src/Components/Photography.jsx
+++ src/Components/Photography.jsx
@@ -8,8 +8,10 @@ function Photography() {
        <p className="group-hover:bg-[#7a78ff] opacity-45 group-hover:w-full group-hover:h-full absolute top-0 left-0"></p>
        <p className=" absolute top-14 left-44 text-white text-3xl group-hover:scale-125">Photography</p>
        </div>
-       <div className=" h-[300px]">
+       <div className=" h-[300px] relative group">
        <img src="https://images.squarespace-cdn.com/content/v1/5b5617b9697a9878c6351a85/1604434544631-T821ET958H8RNV5ROWO0/Cheetah.jpg?format=1000w" alt="" className=" h-full"  />
+       <p className="group-hover:bg-[#ffb233] opacity-45 group-hover:w-full group-hover:h-full absolute top-0 left-0"></p>
+       <p className=" absolute top-14 left-44 text-white text-3xl group-hover:scale-125">Our Click</p>
        </div>
     </div>
    </section>
