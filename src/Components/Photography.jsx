dd1344c5d1d84dde34a706ed78f7f2cfc349f642
Jibon <manvirjibon@gmail.com>
2023-12-29 19:04:12 +0600

    group-hovwr

--- src/Components/Photography.jsx
+++ src/Components/Photography.jsx
@@ -3,9 +3,10 @@ function Photography() {
   return (
    <section>
     <div className=" flex  justify-center mt-20">
-       <div className=" h-[300px] relative">
+       <div className=" h-[300px] relative group ">
        <img src="https://images.squarespace-cdn.com/content/v1/5b5617b9697a9878c6351a85/1604434623934-V4M6DMF2VV5627U4RR9D/Pavel_Bolo_Photography.jpg?format=1000w" alt="" className=" h-full" /> 
-       <p className=" absolute top-12 left-44 text-white text-2xl">Photography</p>
+       <p className="group-hover:bg-[#7a78ff] opacity-45 group-hover:w-full group-hover:h-full absolute top-0 left-0"></p>
+       <p className=" absolute top-14 left-44 text-white text-3xl group-hover:scale-125">Photography</p>
        </div>
        <div className=" h-[300px]">
        <img src="https://images.squarespace-cdn.com/content/v1/5b5617b9697a9878c6351a85/1604434544631-T821ET958H8RNV5ROWO0/Cheetah.jpg?format=1000w" alt="" className=" h-full"  />
