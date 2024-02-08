2dff8467172f226dc09cbc7d69cfdab60cf14e8b
Jibon <manvirjibon@gmail.com>
2024-02-08 12:53:03 +0600

    photos show

--- src/Components/Photography.jsx
+++ src/Components/Photography.jsx
@@ -1,8 +1,11 @@
 
 function Photography() {
+  const handleClick =()=>{
+    console.log("data");
+  }
   return (
    <section className=" pb-20">
-    <div className=" flex  justify-center mt-20">
+    <div onClick={handleClick} className=" flex  justify-center mt-20">
        <div className=" h-[350px] relative group ">
        <img src="https://images.squarespace-cdn.com/content/v1/5b5617b9697a9878c6351a85/1604434623934-V4M6DMF2VV5627U4RR9D/Pavel_Bolo_Photography.jpg?format=1000w" alt="" className=" h-full" /> 
        <p className="group-hover:bg-[#7a78ff] opacity-45 group-hover:w-full group-hover:h-full absolute top-0 left-0"></p>
