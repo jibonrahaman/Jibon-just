ff82958897e7f1c4d8b01255c8d7dcbde6072ab2
Jibon <manvirjibon@gmail.com>
2024-02-08 13:04:01 +0600

    design photos page

--- src/Pages/Photos/Photos.jsx
+++ src/Pages/Photos/Photos.jsx
@@ -1,9 +1,11 @@
-
+import A from '../../assets/A.jpg'
 
 export default function Photos() {
   return (
-    <div>
-      
-    </div>
+  <section className=" max-w-container mx-auto"> 
+  <div className=" items-center bg-[#5956ff] px-4 flex  flex-wrap gap-x-3">
+     <img src={A} alt={A} />
+  </div>
+  </section>
   )
 }
