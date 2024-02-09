573bce92fcb03869e837973237c8db4e7a1d5c29
Jibon <manvirjibon@gmail.com>
2024-02-09 11:22:59 +0600

    public folder photo shift

--- src/Pages/Photos/Photos.jsx
+++ src/Pages/Photos/Photos.jsx
@@ -1,38 +1,42 @@
 import Images from '../../Common/Images'
-import A from '../../assets/A.jpg'
-import B from '../../assets/B.jpg'
-import C from '../../assets/C.jpg'
-import D from '../../assets/D.jpg'
-import E from '../../assets/E.jpg'
-import F from '../../assets/F.jpg'
-import G from '../../assets/G.jpg'
-import H from '../../assets/H.jpg'
-import I from '../../assets/I.jpg'
-import J from '../../assets/J.jpg'
-import K from '../../assets/K.jpg'
-import L from '../../assets/L.jpg'
-import M from '../../assets/M.jpg'
-import N from '../../assets/N.jpg'
-import O from '../../assets/O.jpg'
-import P from '../../assets/P.jpg'
-import Q from '../../assets/Q.jpg'
-import R from '../../assets/R.jpg'
-import S from '../../assets/S.jpg'
-import T from '../../assets/T.jpg'
-import U from '../../assets/U.jpg'
-import V from '../../assets/V.jpg'
-import W from '../../assets/W.jpg'
-import X from '../../assets/X.jpg'
-import Y from '../../assets/Y.jpg'
-import Z from '../../assets/Z.jpg'
-import AA from '../../assets/AA.jpg'
+import A from '../../../public/A.jpg'
+import B from '../../../public/B.jpg'
+import C from '../../../public/C.jpg'
+import D from '../../../public/D.jpg'
+import E from '../../../public/E.jpg'
+import F from '../../../public/F.jpg'
+import G from '../../../public/G.jpg'
+import H from '../../../public/H.jpg'
+import I from '../../../public/I.jpg'
+import J from '../../../public/J.jpg'
+import K from '../../../public/K.jpg'
+import L from '../../../public/L.jpg'
+import M from '../../../public/M.jpg'
+import N from '../../../public/N.jpg'
+import O from '../../../public/O.jpg'
+import P from '../../../public/P.jpg'
+import Q from '../../../public/Q.jpg'
+import R from '../../../public/R.jpg'
+import S from '../../../public/S.jpg'
+import T from '../../../public/T.jpg'
+import U from '../../../public/U.jpg'
+import V from '../../../public/V.jpg'
+import W from '../../../public/W.jpg'
+import X from '../../../public/X.jpg'
+import Y from '../../../public/Y.jpg'
+import Z from '../../../public/Z.jpg'
+import AA from '../../../public/AA.jpg'
 
 
 
+import { IoReturnUpBackSharp } from "react-icons/io5";
 
 export default function Photos() {
   return (
   <section className=" flex justify-center bg-[#474777] "> 
+  <div>
+  <IoReturnUpBackSharp />
+  </div>
  <div className=' relative  justify-evenly w-[80%] mt-20 group   gap-y-10  flex flex-wrap' >
 
 <Images src={A} alt={A}  />
