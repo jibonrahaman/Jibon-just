9a42b7b5cf962de68517f8c72966fc4c634cc299
Jibon <manvirjibon@gmail.com>
2024-02-10 15:15:36 +0600

    mixitup btn onclick

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,7 +1,16 @@
 import MixiButton from "../Common/MixiButton"
 import Navbar from "./Navbar"
 import p1 from '../../public/images/p1.png'
+// import { useState } from "react"
+// import skill from "../Skill"
+
 function Project() {
+    // const [data,setData]=useState(skill)
+    // const filt = data.filter
+
+    const handleSubmit =(text)=>{
+        console.log(text);
+    }
   
     return (
         <section className=" max-w-container mx-auto">
@@ -9,11 +18,11 @@ function Project() {
             <div className=" flex gap-x-7 ">
                 <button className="px-4 py-1 rounded-md hover:bg-[#2b4b65] bg-[#0171cd] text-white">ALL</button>
 
-                <MixiButton  text="REACT JS" />
-                <MixiButton text="TAILWIND" />
-                <MixiButton text="BOOTSTRAP" />
-                <MixiButton text="JAVASCRIPT" />
-                <MixiButton text="DAYNAMIC" />
+                <MixiButton onclick={()=>handleSubmit("REACT JS")}  text="REACT JS" />
+                <MixiButton onclick={()=>handleSubmit("TAILWIND")} text="TAILWIND" />
+                <MixiButton onclick={()=>handleSubmit("BOOTSTRAP")} text="BOOTSTRAP" />
+             <MixiButton onclick={()=>handleSubmit("JAVASCRIPT")} text="JAVASCRIPT" />
+                <MixiButton onclick={()=>handleSubmit("DAYNAMIC")} text="DAYNAMIC" />
             </div>
 
             <div className=" mt-5    grid grid-cols-3 gap-x-8 ">
@@ -22,106 +31,14 @@ function Project() {
             <div className="">
                 <img src={p1} alt="" />
             <div className=" bg-[#d9eeff] p-3 ">
-           <h2 className=" text-xl font-semibold">ChitChat Chating App</h2> 
-           <p className=" mt-3">On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.</p> 
+           <h2 className=" text-xl font-semibold"></h2> 
+           <p className=" mt-3"></p> 
            <p className=" items-center mt-3">Technology:
-            <button className="px-3  cursor-pointer bg-white text-black ml-3 rounded-md">HTML5
+            <button className="px-3  cursor-pointer bg-white text-black ml-3 rounded-md">
             </button> 
-            <button className="px-3  cursor-pointer bg-white text-black ml-3 rounded-md">CSS3
+            <button className="px-3  cursor-pointer bg-white text-black ml-3 rounded-md">
             </button> 
-            <button className="px-3  cursor-pointer bg-white text-black ml-3 rounded-md">Js
-            </button>
-            </p>  
-            <div className=" flex gap-x-7 mt-6"> 
-            <button className=" px-3 py-1 bg-[#0171cd] rounded-md text-white border border-[#0171cd] hover:bg-transparent duration-300 hover:text-[#0171cd] font-semibold">LIVE
-            </button>
-            <button className=" px-3 py-1 hover:bg-[#0171cd] rounded-md hover:text-white border border-[#0171cd] bg-transparent duration-300 text-[#0171cd]  font-semibold">GITHUB
-            </button>
-         </div>   
-           </div>
-              </div>
-    {/* card design */}   {/* card design */}
- 
-            <div className="">
-                <img src={p1} alt="" />
-            <div className=" bg-[#d9eeff] p-3 ">
-           <h2 className=" text-xl font-semibold">ChitChat Chating App</h2> 
-           <p className=" mt-3">On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.</p> 
-           <p className=" items-center mt-3">Technology:
-            <button className="px-3  cursor-pointer bg-white text-black ml-3 rounded-md">HTML5
-            </button> 
-            <button className="px-3  cursor-pointer bg-white text-black ml-3 rounded-md">CSS3
-            </button> 
-            <button className="px-3  cursor-pointer bg-white text-black ml-3 rounded-md">Js
-            </button>
-            </p>  
-            <div className=" flex gap-x-7 mt-6"> 
-            <button className=" px-3 py-1 bg-[#0171cd] rounded-md text-white border border-[#0171cd] hover:bg-transparent duration-300 hover:text-[#0171cd] font-semibold">LIVE
-            </button>
-            <button className=" px-3 py-1 hover:bg-[#0171cd] rounded-md hover:text-white border border-[#0171cd] bg-transparent duration-300 text-[#0171cd]  font-semibold">GITHUB
-            </button>
-         </div>   
-           </div>
-              </div>
-    {/* card design */}   {/* card design */}
- 
-            <div className="">
-                <img src={p1} alt="" />
-            <div className=" bg-[#d9eeff] p-3 ">
-           <h2 className=" text-xl font-semibold">ChitChat Chating App</h2> 
-           <p className=" mt-3">On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.</p> 
-           <p className=" items-center mt-3">Technology:
-            <button className="px-3  cursor-pointer bg-white text-black ml-3 rounded-md">HTML5
-            </button> 
-            <button className="px-3  cursor-pointer bg-white text-black ml-3 rounded-md">CSS3
-            </button> 
-            <button className="px-3  cursor-pointer bg-white text-black ml-3 rounded-md">Js
-            </button>
-            </p>  
-            <div className=" flex gap-x-7 mt-6"> 
-            <button className=" px-3 py-1 bg-[#0171cd] rounded-md text-white border border-[#0171cd] hover:bg-transparent duration-300 hover:text-[#0171cd] font-semibold">LIVE
-            </button>
-            <button className=" px-3 py-1 hover:bg-[#0171cd] rounded-md hover:text-white border border-[#0171cd] bg-transparent duration-300 text-[#0171cd]  font-semibold">GITHUB
-            </button>
-         </div>   
-           </div>
-              </div>
-    {/* card design */}   {/* card design */}
- 
-            <div className="">
-                <img src={p1} alt="" />
-            <div className=" bg-[#d9eeff] p-3 ">
-           <h2 className=" text-xl font-semibold">ChitChat Chating App</h2> 
-           <p className=" mt-3">On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.</p> 
-           <p className=" items-center mt-3">Technology:
-            <button className="px-3  cursor-pointer bg-white text-black ml-3 rounded-md">HTML5
-            </button> 
-            <button className="px-3  cursor-pointer bg-white text-black ml-3 rounded-md">CSS3
-            </button> 
-            <button className="px-3  cursor-pointer bg-white text-black ml-3 rounded-md">Js
-            </button>
-            </p>  
-            <div className=" flex gap-x-7 mt-6"> 
-            <button className=" px-3 py-1 bg-[#0171cd] rounded-md text-white border border-[#0171cd] hover:bg-transparent duration-300 hover:text-[#0171cd] font-semibold">LIVE
-            </button>
-            <button className=" px-3 py-1 hover:bg-[#0171cd] rounded-md hover:text-white border border-[#0171cd] bg-transparent duration-300 text-[#0171cd]  font-semibold">GITHUB
-            </button>
-         </div>   
-           </div>
-              </div>
-    {/* card design */}   {/* card design */}
- 
-            <div className="">
-                <img src={p1} alt="" />
-            <div className=" bg-[#d9eeff] p-3 ">
-           <h2 className=" text-xl font-semibold">ChitChat Chating App</h2> 
-           <p className=" mt-3">On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.</p> 
-           <p className=" items-center mt-3">Technology:
-            <button className="px-3  cursor-pointer bg-white text-black ml-3 rounded-md">HTML5
-            </button> 
-            <button className="px-3  cursor-pointer bg-white text-black ml-3 rounded-md">CSS3
-            </button> 
-            <button className="px-3  cursor-pointer bg-white text-black ml-3 rounded-md">Js
+            <button className="px-3  cursor-pointer bg-white text-black ml-3 rounded-md">
             </button>
             </p>  
             <div className=" flex gap-x-7 mt-6"> 
--- src/Skill.js
+++ src/Skill.js
@@ -1,4 +1,17 @@
 const skill = [
-
-]
-export default skill;
+    {
+      "id": '1',
+      "imgs": "../../public/images/p1",
+      "webName": "ChitChat Chating App",
+      "paragraph": "On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.",
+      "html": "HTML5",
+      "css": "CSS3",
+      "js": "JS",
+      "react": "REACT",
+      "tailwind": "TAILWIND",
+      "title": ["TAILWIND", "REACT JS", "DAYNAMIC"]
+    }
+  ];
+  
+  export default skill;
+  
