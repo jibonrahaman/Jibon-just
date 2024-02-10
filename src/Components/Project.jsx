737f11e500879402b81c311a91c2986e37a6b421
Jibon <manvirjibon@gmail.com>
2024-02-10 12:59:35 +0600

    mixibtn done

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,20 +1,22 @@
+import MixiButton from "../Common/MixiButton"
 import Navbar from "./Navbar"
 
 function Project() {
-  return (
-  <section className=" max-w-container mx-auto">
-    <Navbar/>
-    <div className=" flex gap-x-7 ">
-        <button className="px-4 py-1 rounded-md hover:bg-[#2b4b65] bg-[#0171cd] text-white">ALL</button>
-       
+  
+    return (
+        <section className=" max-w-container mx-auto">
+            <Navbar />
+            <div className=" flex gap-x-7 ">
+                <button className="px-4 py-1 rounded-md hover:bg-[#2b4b65] bg-[#0171cd] text-white">ALL</button>
 
-        <button>TAILWIND</button>
-        <button>BOOTSTRAP</button>
-        <button>JAVASCRIPT</button>
-        <button>DAYNAMIC</button>
-    </div>
-  </section>
-  )
+                <MixiButton  text="REACT JS" />
+                <MixiButton text="TAILWIND" />
+                <MixiButton text="BOOTSTRAP" />
+                <MixiButton text="JAVASCRIPT" />
+                <MixiButton text="DAYNAMIC" />
+            </div>
+        </section>
+    )
 }
 
 export default Project
