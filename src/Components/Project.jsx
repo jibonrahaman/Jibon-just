58b8d8f03a5635f0b1514ae461dd1b71e9064ebd
Jibon <manvirjibon@gmail.com>
2024-02-10 11:32:10 +0600

    mixitup design

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,8 +1,10 @@
+import Navbar from "./Navbar"
 
 function Project() {
   return (
-  <section>
-    <div>
+  <section className=" max-w-container mx-auto">
+    <Navbar/>
+    <div className=" flex gap-x-9">
         <button>ALL</button>
         <button>REACT JS</button>
         <button>TAILWIND</button>
