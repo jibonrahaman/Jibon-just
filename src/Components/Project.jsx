c7fa50b68f06468d5012638dc61014bcaf8670b4
Jibon <manvirjibon@gmail.com>
2024-02-10 15:55:56 +0600

    data fetch onlcik

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,22 +1,31 @@
 import MixiButton from "../Common/MixiButton"
 import Navbar from "./Navbar"
 import p1 from '../../public/images/p1.png'
-// import { useState } from "react"
-// import skill from "../Skill"
+import { useState } from "react"
+import skill from "../Skill"
 
 function Project() {
-    // const [data,setData]=useState(skill)
-    // const filt = data.filter
+    const [data,setData]=useState(skill)
+    const [filterProtfolio,setFilterProtfolio] = useState(skill)
 
     const handleSubmit =(text)=>{
-        console.log(text);
+        let arr = []
+        const filt = data.filter((item)=>{
+         item.title.filter((item2)=>{
+            if(item2 == text){
+              arr.push(item)
+            }
+         })
+         
+        })
+       setFilterProtfolio(arr)        
     }
   
     return (
         <section className=" max-w-container mx-auto">
             <Navbar />
             <div className=" flex gap-x-7 ">
-                <button className="px-4 py-1 rounded-md hover:bg-[#2b4b65] bg-[#0171cd] text-white">ALL</button>
+                <button onClick={()=>{setFilterProtfolio(skill)}} className="px-4 py-1 rounded-md hover:bg-[#2b4b65] bg-[#0171cd] text-white" >ALL</button>
 
                 <MixiButton onclick={()=>handleSubmit("REACT JS")}  text="REACT JS" />
                 <MixiButton onclick={()=>handleSubmit("TAILWIND")} text="TAILWIND" />
@@ -28,27 +37,30 @@ function Project() {
             <div className=" mt-5    grid grid-cols-3 gap-x-8 ">
        {/* card design */}
  
-            <div className="">
-                <img src={p1} alt="" />
-            <div className=" bg-[#d9eeff] p-3 ">
-           <h2 className=" text-xl font-semibold"></h2> 
-           <p className=" mt-3"></p> 
-           <p className=" items-center mt-3">Technology:
-            <button className="px-3  cursor-pointer bg-white text-black ml-3 rounded-md">
-            </button> 
-            <button className="px-3  cursor-pointer bg-white text-black ml-3 rounded-md">
-            </button> 
-            <button className="px-3  cursor-pointer bg-white text-black ml-3 rounded-md">
-            </button>
-            </p>  
-            <div className=" flex gap-x-7 mt-6"> 
-            <button className=" px-3 py-1 bg-[#0171cd] rounded-md text-white border border-[#0171cd] hover:bg-transparent duration-300 hover:text-[#0171cd] font-semibold">LIVE
-            </button>
-            <button className=" px-3 py-1 hover:bg-[#0171cd] rounded-md hover:text-white border border-[#0171cd] bg-transparent duration-300 text-[#0171cd]  font-semibold">GITHUB
-            </button>
-         </div>   
-           </div>
-              </div>
+            {
+                filterProtfolio.map((item,id)=>{
+                   return <div key={id}>
+                    <img src={item.imgs} alt="" />
+                <div className=" bg-[#d9eeff] p-3 ">
+               <h2 className=" text-xl font-semibold">{item.webName}</h2> 
+               <p className=" mt-3">{item.paragraph}</p> 
+               <p className=" items-center mt-3">Technology:
+                <button className="px-3  cursor-pointer bg-white text-black ml-3 rounded-md">{item.html}</button> 
+                <button className="px-3  cursor-pointer bg-white text-black ml-3 rounded-md">{item.css}</button> 
+                <button className="px-3  cursor-pointer bg-white text-black ml-3 rounded-md">{item.js}</button> 
+                <button className="px-3  cursor-pointer bg-white text-black ml-3 rounded-md">{item.react}</button>
+                <button className="px-3 mt-2  cursor-pointer bg-white text-black ml-3 rounded-md">{item.tailwind}</button>
+                </p>  
+                <div className=" flex gap-x-7 mt-6"> 
+                <button className=" px-3 py-1 bg-[#0171cd] rounded-md text-white border border-[#0171cd] hover:bg-transparent duration-300 hover:text-[#0171cd] font-semibold">LIVE
+                </button>
+                <button className=" px-3 py-1 hover:bg-[#0171cd] rounded-md hover:text-white border border-[#0171cd] bg-transparent duration-300 text-[#0171cd]  font-semibold">GITHUB
+                </button>
+             </div>   
+               </div>
+                  </div>
+                })
+            }
     {/* card design */}
            </div>             
                  </section>
--- src/Skill.js
+++ src/Skill.js
@@ -1,7 +1,7 @@
 const skill = [
     {
       "id": '1',
-      "imgs": "../../public/images/p1",
+      "imgs": "../../public/images/p1.png",
       "webName": "ChitChat Chating App",
       "paragraph": "On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.",
       "html": "HTML5",
