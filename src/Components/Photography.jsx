d78f321a376d3db6f34027c2b67f0d9957e1aa4e
Jibon <manvirjibon@gmail.com>
2024-02-10 11:15:04 +0600

    changing name

--- src/Components/Photography.jsx
+++ src/Components/Photography.jsx
@@ -17,7 +17,7 @@ function Photography() {
     <div className=" h-[350px] relative group">
        <img src="https://images.squarespace-cdn.com/content/v1/5b5617b9697a9878c6351a85/1604434544631-T821ET958H8RNV5ROWO0/Cheetah.jpg?format=1000w" alt="" className=" h-full"  />
        <p className="group-hover:bg-[#ffb233] opacity-45 group-hover:w-full group-hover:h-full absolute top-0 left-0"></p>
-       <p className=" absolute top-14 left-44 text-white text-3xl group-hover:scale-125">Our Click</p>
+       <p className=" absolute top-14 left-44 text-white text-3xl group-hover:scale-125">My Project</p>
        </div>
     </Link>
     </div>
