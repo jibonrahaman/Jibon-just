08bceee1d40264531577f6bee6e39ab37cf85558
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-02-13 13:08:49 +0600

    navbar responsive

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,8 +3,8 @@ import logo from '../assets/jibon.png'
 function Navbar() {
   return (
     <section className=''>
-      <div className=' flex md:justify-between items-center'>
-        <img src={logo} alt="" className='w-48 h-[120px] md:w-64 md:h-40 ' />
+      <div className=' flex justify-center md:justify-between items-center'>
+        <img src={logo} alt="" className='w-48 mt-2 md:mt-0 md:w-64 md:h-40 ' />
         <div className=' flex font-semibold gap-x-5'>
           <Link to="/">
             <p>Home</p>
