bedc6afc343a92ecfc62b53178cc3cf51eb8453a
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-02-13 20:37:40 +0600

    issue solved

--- src/Common/Images.jsx
+++ src/Common/Images.jsx
@@ -5,7 +5,7 @@ AOS.init();
 
 function Images( {src,alt,className}) {
   return (
-<div className="md:w-[45%] md:h-[400px] w-[300px] h-[250px]  relative" data-aos="zoom-in-up" >
+<div className="md:w-[45%] md:h-[400px] w-[250px] h-[250px]  relative" data-aos="zoom-in-up" >
 <img src={src} alt={alt} className={` rounded-lg w-full bg-black/70 hover:opacity-50 h-full  object-cover ${className}`} />
 
 </div>
