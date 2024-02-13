2051f6319a611807cd53fe2fac3f9f6b5f9ce326
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-02-13 12:54:53 +0600

    responsive

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,10 +2,10 @@ import { Link } from 'react-router-dom'
 import logo from '../assets/jibon.png'
 function Navbar() {
   return (
-    <section>
-      <div className=' flex justify-between items-center'>
-        <img src={logo} alt="" className=' w-64 h-40 ' />
-        <div className=' flex font-semibold gap-x-5'>
+    <section className=''>
+      <div className=' flex md:justify-between items-center'>
+        <img src={logo} alt="" className=' w-48 md:w-64 h-40 ' />
+        <div className=' flex font-semibold md:gap-x-5'>
           <Link to="/">
             <p>Home</p>
           </Link>
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -7,11 +7,11 @@ AOS.init();
 
 export default function HomePage() {
   return (
-    <section className="max-w-container mx-auto" data-aos="fade-down"
+    <section className="max-w-container md:mx-auto" data-aos="fade-down"
     data-aos-easing="linear"
     data-aos-duration="1000">
       <Navbar />
-      <p className=" w-[1100px] text-center text-4xl mx-auto mt-14">
+      <p className=" md:w-[1100px] text-center text-4xl mx-auto md:mt-14">
         <span className=" font-bold">Hi, I’m Jibon :)  </span>
         I’m a web designer  and web application devloper. I foster innovative teams that create meaningful experiences. Below are some of the projects I worked on lately.
       </p>
