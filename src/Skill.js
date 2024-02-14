761043eda82ff7dfd5b13daa7b627c611244ac0d
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-02-14 11:44:34 +0600

    s

--- src/Skill.js
+++ src/Skill.js
@@ -11,231 +11,231 @@ const skill = [
       "tailwind": "TAILWIND",
       "title": ["TAILWIND", "REACT JS", "DAYNAMIC"]
     },
-    {
-      "id": '2',
-      "imgs": "../../public/images/p2.png",
-      "webName": "Orebi E-Commerce",
-      "paragraph": "On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.",
-      "html": "HTML5",
-      "css": "CSS3",
-      "js": "JS",
-      "react": "REACT",
-      "tailwind": "TAILWIND",
-      "redux": "REDUX",
-      "title": ["TAILWIND", "REACT JS", "DAYNAMIC"]
-    },
-    {
-      "id": '3',
-      "imgs": "../../public/images/p3.png",
-      "webName": "My Portfolio",
-      "paragraph": "On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.",
-      "html": "HTML5",
-      "css": "CSS3",
-      "js": "JS",
-      "react": "REACT",
-      "tailwind": "TAILWIND",
-      "redux": "REDUX",
-      "title": [ "TAILWIND","DAYNAMIC","REACT JS"]
-    },
-    {
-      "id": '4',
-      "imgs": "../../public/images/p4.png",
-      "webName": "OMAH",
-      "paragraph": "This is a Aaricle Website. The website is fully responsive. Created the website to practice Sass. I have used HTML, CSS and Sass to create this website.",
-      "html": "HTML5",
-      "css": "CSS3",     
-      "title": ["BOOTSTRAP"]
-    }, 
-    {
-      "id": '5',
-      "imgs": "../../public/images/p5.png",
-      "webName": "QuiriBox",
-      "paragraph": "This is a Aaricle Website. The website is fully responsive. Created the website to practice Sass. I have used HTML, CSS and Sass to create this website.",
-      "html": "HTML5",
-      "css": "CSS3",     
-      "title": ["BOOTSTRAP"]
-    },
-    {
-      "id": '6',
-      "imgs": "../../public/images/p6.png",
-      "webName": "PETROIL",
-      "paragraph": "This is a Aaricle Website. The website is fully responsive. Created the website to practice Sass. I have used HTML, CSS and Sass to create this website.",
-      "html": "HTML5",
-      "css": "CSS3",     
-      "title": ["BOOTSTRAP"]
-    },
-    {
-      "id": '7',
-      "imgs": "../../public/images/p7.png",
-      "webName": "MixiTup",
-      "paragraph": "This is a Aaricle Website. The website is fully responsive. Created the website to practice Sass. I have used HTML, CSS and Sass to create this website.",
-      "tailwind": "TAILWIND",   
-      "react": "REACT", 
-      "title": [ "REACT JS","TAILWIND" ]
-    }, 
-      {
-      "id": '8',
-      "imgs": "../../public/images/p8.png",
-      "webName": "Instagram Clone",
-      "paragraph": "On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.",
-      "html": "HTML5",
-      "css": "CSS3",
-      "js": "JS",
-      "react": "REACT",
-      "tailwind": "TAILWIND",
-      "title": [ "DAYNAMIC","TAILWIND", "REACT JS"]
-    },
-      {
-      "id": '9',
-      "imgs": "../../public/images/p9.png",
-      "webName": "Javascript Todo",
-      "paragraph": "On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.",
-      "html": "HTML5",
-      "css": "CSS3",
-      "js": "JS",    
-      "title": ["JAVASCRIPT"]
-    },{
-      "id": '10',
-      "imgs": "../../public/images/p10.png",
-      "webName": "Javascript Todo Complex",
-      "paragraph": "On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.",
-      "html": "HTML5",
-      "css": "CSS3",
-      "js": "JS",    
-      "title": ["JAVASCRIPT"]
-    }, {
-      "id": '11',
-      "imgs": "../../public/images/p11.png",
-      "webName": "Javascript Slider",
-      "paragraph": "On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.",
-      "html": "HTML5",
-      "css": "CSS3",
-      "js": "JS",    
-      "title": ["JAVASCRIPT"]
-    },
-     {
-      "id": '12',
-      "imgs": "../../public/images/p12.png",
-      "webName": "Redux Signup Login Dashboard",
-      "paragraph": "On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.",
-      "react": "REACT",
-      "tailwind": "TAILWIND",
-      "redux": "REDUX",    
-      "title": [ "REACT JS","TAILWIND"]
-    },
-    {
-      "id": '13',
-      "imgs": "../../public/images/p13.png",
-      "webName": "Love Website",
-      "paragraph": "On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.",
-      "html": "HTML5",
-      "css": "CSS3",
-      "js": "JS",    
-      "title": ["BOOTSTRAP"]
-    },
-      {
-      "id": '14',
-      "imgs": "../../public/images/p14.png",
-      "webName": "FinsWet",
-      "paragraph": "On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.",
-      "html": "HTML5",
-      "css": "CSS3",
-      "js": "JS",    
-      "title": ["BOOTSTRAP"]
-    }, 
-      {
-      "id": '15',
-      "imgs": "../../public/images/p15.png",
-      "webName": "Query",
-      "paragraph": "On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.",
-      "html": "HTML5",
-      "css": "CSS3",
-      "js": "JS",    
-      "title": ["BOOTSTRAP"]
-    },
-     {
-      "id": '16',
-      "imgs": "../../public/images/p16.png",
-      "webName": "Innovate",
-      "paragraph": "On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.",
-      "html": "HTML5",
-      "css": "CSS3",
-      "js": "JS",    
-      "title": ["BOOTSTRAP"]
-    },
-     {
-      "id": '17',
-      "imgs": "../../public/images/p17.png",
-      "webName": "Demo Portfolio",
-      "paragraph": "On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.",
-      "html": "HTML5",
-      "css": "CSS3",
-      "js": "JS", 
-       "react": "REACT",
-      "tailwind": "TAILWIND",
-      "redux": "REDUX",   
-      "title": ["DAYNAMIC","TAILWIND", "REACT JS" ]
-    },
-     {
-      "id": '18',
-      "imgs": "../../public/images/p18.png",
-      "webName": "Toggle React",
-      "paragraph": "On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.",
-      "html": "HTML5",
-      "css": "CSS3",
-      "js": "JS", 
-       "react": "REACT",
-      "tailwind": "TAILWIND",    
-      "title": ["REACT JS","TAILWIND"]
-    }, {
-      "id": '19',
-      "imgs": "../../public/images/p19.png",
-      "webName": "Guessing Game Javascript",
-      "paragraph": "On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.",
-      "html": "HTML5",
-      "css": "CSS3",
-      "js": "JS",    
-      "title": ["JAVASCRIPT"]
-    },
-    {
-      "id": '20',
-      "imgs": "../../public/images/p20.png",
-      "webName": "Toggle React",
-      "paragraph": "On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.",
-      "html": "HTML5",
-      "css": "CSS3",
-      "js": "JS", 
-       "react": "REACT",
-      "tailwind": "TAILWIND",    
-      "title": ["TAILWIND", "REACT JS"]
-    }, {
-      "id": '21',
-      "imgs": "../../public/images/p21.png",
-      "webName": "Pet Store And Beyond",
-      "paragraph": "On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.",
-      "html": "HTML5",
-      "css": "CSS3",
-      "js": "JS",    
-      "title": ["BOOTSTRAP"]
-    }, 
-    {
-      "id": '22',
-      "imgs": "../../public/images/p22.png",
-      "webName": "Colour Meaching",
-      "paragraph": "On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.",
-      "html": "HTML5",
-      "css": "CSS3",   
-      "title": ["BOOTSTRAP"]
-    }, 
-    {
-      "id": '23',
-      "imgs": "../../public/images/p23.png",
-      "webName": "E-School",
-      "paragraph": "On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.",
-      "html": "HTML5",
-      "css": "CSS3",   
-      "title": ["BOOTSTRAP"]
-    },
+    // {
+    //   "id": '2',
+    //   "imgs": "../../public/images/p2.png",
+    //   "webName": "Orebi E-Commerce",
+    //   "paragraph": "On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.",
+    //   "html": "HTML5",
+    //   "css": "CSS3",
+    //   "js": "JS",
+    //   "react": "REACT",
+    //   "tailwind": "TAILWIND",
+    //   "redux": "REDUX",
+    //   "title": ["TAILWIND", "REACT JS", "DAYNAMIC"]
+    // },
+    // {
+    //   "id": '3',
+    //   "imgs": "../../public/images/p3.png",
+    //   "webName": "My Portfolio",
+    //   "paragraph": "On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.",
+    //   "html": "HTML5",
+    //   "css": "CSS3",
+    //   "js": "JS",
+    //   "react": "REACT",
+    //   "tailwind": "TAILWIND",
+    //   "redux": "REDUX",
+    //   "title": [ "TAILWIND","DAYNAMIC","REACT JS"]
+    // },
+    // {
+    //   "id": '4',
+    //   "imgs": "../../public/images/p4.png",
+    //   "webName": "OMAH",
+    //   "paragraph": "This is a Aaricle Website. The website is fully responsive. Created the website to practice Sass. I have used HTML, CSS and Sass to create this website.",
+    //   "html": "HTML5",
+    //   "css": "CSS3",     
+    //   "title": ["BOOTSTRAP"]
+    // }, 
+    // {
+    //   "id": '5',
+    //   "imgs": "../../public/images/p5.png",
+    //   "webName": "QuiriBox",
+    //   "paragraph": "This is a Aaricle Website. The website is fully responsive. Created the website to practice Sass. I have used HTML, CSS and Sass to create this website.",
+    //   "html": "HTML5",
+    //   "css": "CSS3",     
+    //   "title": ["BOOTSTRAP"]
+    // },
+    // {
+    //   "id": '6',
+    //   "imgs": "../../public/images/p6.png",
+    //   "webName": "PETROIL",
+    //   "paragraph": "This is a Aaricle Website. The website is fully responsive. Created the website to practice Sass. I have used HTML, CSS and Sass to create this website.",
+    //   "html": "HTML5",
+    //   "css": "CSS3",     
+    //   "title": ["BOOTSTRAP"]
+    // },
+    // {
+    //   "id": '7',
+    //   "imgs": "../../public/images/p7.png",
+    //   "webName": "MixiTup",
+    //   "paragraph": "This is a Aaricle Website. The website is fully responsive. Created the website to practice Sass. I have used HTML, CSS and Sass to create this website.",
+    //   "tailwind": "TAILWIND",   
+    //   "react": "REACT", 
+    //   "title": [ "REACT JS","TAILWIND" ]
+    // }, 
+    //   {
+    //   "id": '8',
+    //   "imgs": "../../public/images/p8.png",
+    //   "webName": "Instagram Clone",
+    //   "paragraph": "On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.",
+    //   "html": "HTML5",
+    //   "css": "CSS3",
+    //   "js": "JS",
+    //   "react": "REACT",
+    //   "tailwind": "TAILWIND",
+    //   "title": [ "DAYNAMIC","TAILWIND", "REACT JS"]
+    // },
+    //   {
+    //   "id": '9',
+    //   "imgs": "../../public/images/p9.png",
+    //   "webName": "Javascript Todo",
+    //   "paragraph": "On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.",
+    //   "html": "HTML5",
+    //   "css": "CSS3",
+    //   "js": "JS",    
+    //   "title": ["JAVASCRIPT"]
+    // },{
+    //   "id": '10',
+    //   "imgs": "../../public/images/p10.png",
+    //   "webName": "Javascript Todo Complex",
+    //   "paragraph": "On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.",
+    //   "html": "HTML5",
+    //   "css": "CSS3",
+    //   "js": "JS",    
+    //   "title": ["JAVASCRIPT"]
+    // }, {
+    //   "id": '11',
+    //   "imgs": "../../public/images/p11.png",
+    //   "webName": "Javascript Slider",
+    //   "paragraph": "On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.",
+    //   "html": "HTML5",
+    //   "css": "CSS3",
+    //   "js": "JS",    
+    //   "title": ["JAVASCRIPT"]
+    // },
+    //  {
+    //   "id": '12',
+    //   "imgs": "../../public/images/p12.png",
+    //   "webName": "Redux Signup Login Dashboard",
+    //   "paragraph": "On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.",
+    //   "react": "REACT",
+    //   "tailwind": "TAILWIND",
+    //   "redux": "REDUX",    
+    //   "title": [ "REACT JS","TAILWIND"]
+    // },
+    // {
+    //   "id": '13',
+    //   "imgs": "../../public/images/p13.png",
+    //   "webName": "Love Website",
+    //   "paragraph": "On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.",
+    //   "html": "HTML5",
+    //   "css": "CSS3",
+    //   "js": "JS",    
+    //   "title": ["BOOTSTRAP"]
+    // },
+    //   {
+    //   "id": '14',
+    //   "imgs": "../../public/images/p14.png",
+    //   "webName": "FinsWet",
+    //   "paragraph": "On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.",
+    //   "html": "HTML5",
+    //   "css": "CSS3",
+    //   "js": "JS",    
+    //   "title": ["BOOTSTRAP"]
+    // }, 
+    //   {
+    //   "id": '15',
+    //   "imgs": "../../public/images/p15.png",
+    //   "webName": "Query",
+    //   "paragraph": "On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.",
+    //   "html": "HTML5",
+    //   "css": "CSS3",
+    //   "js": "JS",    
+    //   "title": ["BOOTSTRAP"]
+    // },
+    //  {
+    //   "id": '16',
+    //   "imgs": "../../public/images/p16.png",
+    //   "webName": "Innovate",
+    //   "paragraph": "On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.",
+    //   "html": "HTML5",
+    //   "css": "CSS3",
+    //   "js": "JS",    
+    //   "title": ["BOOTSTRAP"]
+    // },
+    //  {
+    //   "id": '17',
+    //   "imgs": "../../public/images/p17.png",
+    //   "webName": "Demo Portfolio",
+    //   "paragraph": "On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.",
+    //   "html": "HTML5",
+    //   "css": "CSS3",
+    //   "js": "JS", 
+    //    "react": "REACT",
+    //   "tailwind": "TAILWIND",
+    //   "redux": "REDUX",   
+    //   "title": ["DAYNAMIC","TAILWIND", "REACT JS" ]
+    // },
+    //  {
+    //   "id": '18',
+    //   "imgs": "../../public/images/p18.png",
+    //   "webName": "Toggle React",
+    //   "paragraph": "On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.",
+    //   "html": "HTML5",
+    //   "css": "CSS3",
+    //   "js": "JS", 
+    //    "react": "REACT",
+    //   "tailwind": "TAILWIND",    
+    //   "title": ["REACT JS","TAILWIND"]
+    // }, {
+    //   "id": '19',
+    //   "imgs": "../../public/images/p19.png",
+    //   "webName": "Guessing Game Javascript",
+    //   "paragraph": "On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.",
+    //   "html": "HTML5",
+    //   "css": "CSS3",
+    //   "js": "JS",    
+    //   "title": ["JAVASCRIPT"]
+    // },
+    // {
+    //   "id": '20',
+    //   "imgs": "../../public/images/p20.png",
+    //   "webName": "Toggle React",
+    //   "paragraph": "On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.",
+    //   "html": "HTML5",
+    //   "css": "CSS3",
+    //   "js": "JS", 
+    //    "react": "REACT",
+    //   "tailwind": "TAILWIND",    
+    //   "title": ["TAILWIND", "REACT JS"]
+    // }, {
+    //   "id": '21',
+    //   "imgs": "../../public/images/p21.png",
+    //   "webName": "Pet Store And Beyond",
+    //   "paragraph": "On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.",
+    //   "html": "HTML5",
+    //   "css": "CSS3",
+    //   "js": "JS",    
+    //   "title": ["BOOTSTRAP"]
+    // }, 
+    // {
+    //   "id": '22',
+    //   "imgs": "../../public/images/p22.png",
+    //   "webName": "Colour Meaching",
+    //   "paragraph": "On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.",
+    //   "html": "HTML5",
+    //   "css": "CSS3",   
+    //   "title": ["BOOTSTRAP"]
+    // }, 
+    // {
+    //   "id": '23',
+    //   "imgs": "../../public/images/p23.png",
+    //   "webName": "E-School",
+    //   "paragraph": "On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.",
+    //   "html": "HTML5",
+    //   "css": "CSS3",   
+    //   "title": ["BOOTSTRAP"]
+    // },
   ];
   
   export default skill;
